b4cc8f68ae6e5fa55f1b0fd028d29a33faa21250
asagiv <ari.sagiv@reliacoat.com>
2019-12-17 16:01:11 -0500

    Update LinkedLIst functionality for more generic purposes.

--- LinkedList.py
+++ LinkedList.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class SinglyLinkedList():
     '''Functions involving singly-linked list nodes.'''
 
@@ -9,61 +8,86 @@ class SinglyLinkedList():
         self.tail = None #The tail (last value) of the singly-linked list.
         self.count = 0 #The number of items in the singly-linked list.
 
-    def add(self, value):
-        '''Adds a value to the singly-linked list, and sets it as the tail.'''
-
-        # create new list node
-        nodeToAdd = SinglyLinkedListNode(value)
-
-        # If the linked list is empty, the head and the tail are the newly entered value.
-        if self.head == None:
-            self.head = nodeToAdd
-            self.tail = self.head
-
-        else:
-            self.tail.next = nodeToAdd
-            self.tail = nodeToAdd
-
-        self.count += 1
-
     def insert(self, index, value):
         ''' Inserts the value to the nth index of the linked list. If n is greater than number of items, sets as last value.'''
 
-        # If index is greater than count, set tail to the new value.
-        if(index > self.count):
-            self.add(value)
-            return
-
         newNode = SinglyLinkedListNode(value)
 
+        # Sets the head if the linked list is empty.
+        if self.head == None:
+            self.head = newNode
+            self.tail = self.head
+
+        # If index is greater than count, set tail to the new value.
+        elif(index >= self.count):
+            self.tail.next = newNode
+            self.tail = newNode
+
         # If index is 0, set head to new value.
-        if(index == 0):
+        elif(index == 0):
             newNode.next = self.head
             self.head = newNode
-            return
 
-        node = self.head
+        else:
+            node = self.head
 
-        for i in range(0, index - 1):
-            node = node.next
+            #Iterating to obtain the index node
+            for _ in range(0, index - 1):
+                node = node.next
         
-        newNode.next = node.next
-        node.next = newNode
+            newNode.next = node.next
+            node.next = newNode
 
         self.count += 1
+
+    def append(self, value):
+        ''' appends a value to the singly-linked list, and sets it as the tail.'''
+        self.insert(self.count, value)
+
+    def remove(self, index):
+        ''' removes a value from the linked list based on its index '''
+        if self.head == None:
+            return None
+        
+        # returns none if index is greater than count
+        elif(index > self.count):
+            return None
+
+        # returns head if index = 0
+        elif(index == 0):  
+            node = self.head
+            self.head = node.next
+
+        else:
+            parentNode = self.head
+
+            # iterates through nodes until parent node whose child is index.
+            if(index > 1):
+                for _ in range(0, index - 2):
+                    parentNode = parentNode.next
+
+            node = parentNode.next
+            newNext = node.next
+
+            parentNode.next = newNext
+
+            # sets parent node to tail if parent node's child is the tail.
+            if(newNext == None):
+                self.tail = parentNode
+
+        self.count -= 1
+
+        return node.value
 
     def removeFirst(self):
         '''Removes the first (head) node from the singly-linked list.'''
 
         # if there are no values in the linked list, return.
-        if self.head == None:
-            return
+        return self.remove(0)
 
-        headValue = self.head.value
-
-        self.head = self.head.next
-
-        return headValue
+    def removeLast(self):
+        '''Removes the last (tail) node in the singly-linked list.'''
+        return self.remove(self.count)
 
     def removeFirstEquals(self, valueToRemove):
         '''Removes the first value equal to the input in the singly-linked list.'''
@@ -97,28 +121,6 @@ class SinglyLinkedList():
 
         self.count -= 1
 
-    def removeLast(self):
-        '''Removes the last (tail) node in the singly-linked list.'''
-
-        # If the linked list is empty, return.
-        if self.head == None:
-            return
-
-        currentNode = self.head
-
-        # cannot use tail, requires second-to-last node to set to None
-        for i in range(0, self.count - 2):
-            currentNode = currentNode.next
-
-        nodeToRemove = currentNode.next
-
-        currentNode.next = None
-
-        self.tail = currentNode
-        self.count -= 1
-
-        return nodeToRemove.value
-
     def iterate(self):
         '''Iterates the values in the singly-linked list.'''
         
@@ -139,10 +141,6 @@ class SinglyLinkedList():
 class SinglyLinkedListNode():
     '''A node in a singly-linked list.'''
 
-    def __init__(self):
-        self.value = None # The current value of the node.
+    def __init__(self, value=None):
+        self.value = value # The current value of the node.
         self.next = None #The node that comes after the current node.
-
-    def __init__(self, value):
-        self.value = value
-        self.next = None
--- Main.py
+++ Main.py
@@ -1,47 +1,87 @@
 from LinkedList import SinglyLinkedList
 from LinkedList import SinglyLinkedListNode
 
-linkedList = SinglyLinkedList()
+def createEmptyLinkedList():
+    ''' Creates an empty linked list.'''
+    
+    print('Creating Empty Linked List')
+    
+    linkedList = SinglyLinkedList()
 
-linkedList.add(1)
-linkedList.add(2)
-linkedList.add(3)
-linkedList.add(4)
-linkedList.add(5)
+    return linkedList
+
+def createNewLinkedList():
+    ''' Creates a new linked list: 1 -> 2 -> 3 -> 4 -> 5. '''
+
+    print('Creating a new linked list.')
+
+    linkedList = SinglyLinkedList()
+
+    linkedList.append(1)
+    linkedList.append(2)
+    linkedList.append(3)
+    linkedList.append(4)
+    linkedList.append(5)
+
+    linkedList.iterate()
+
+    return linkedList
+
+# --- Test Insert Operations
+
+# insert into empty array using a random index.
+linkedList = createEmptyLinkedList()
+
+valueToInsert = 6
+indexToInsert = 2
+print('Adding Value to empty Linked List in index {}: {}.'.format(indexToInsert, valueToInsert))
+linkedList.insert(2, valueToInsert)
+linkedList.iterate()
+
+linkedList = createNewLinkedList()
+# inserting a value into the linked list, using previous parameters
+print('Adding Value to index {}: {}.'.format(indexToInsert, valueToInsert))
+linkedList.insert(indexToInsert, valueToInsert)
+linkedList.iterate()
+
+# appending a value
+valueToAdd = 7
+print('Adding Value: {}.'.format(valueToAdd))
+linkedList.append(valueToAdd)
+linkedList.iterate()
+
+# adding to start of linked list
+valueToInsert = 8
+print('Adding value {} to start of linked list.'.format(valueToInsert))
+linkedList.insert(0, valueToInsert)
+linkedList.iterate()
+
+# --- Test Remove Operations
+
+# removing from empty linked list
+linkedList = createEmptyLinkedList()
+indexToRemove = 2
+print('Attempting to remove index {} from empty linked list.'.format(indexToRemove))
+removedValue = linkedList.remove(indexToRemove)
+print('Removed value: {}'.format(removedValue))
+linkedList.iterate()
+
+linkedList = createNewLinkedList()
+# removing a value
+itemToRemove = 2
+print('Removing from linked list index: {}.'.format(itemToRemove))
+linkedList.removeFirstEquals(itemToRemove)
 
 linkedList.iterate()
 
 # removing the last value in the list
 removedValue = linkedList.removeLast()
 print('Removed last value: {}.'.format(removedValue))
-
-linkedList.iterate()
-
-# adding a value
-valueToAdd = 2
-print('Adding Value: {}'.format(valueToAdd))
-linkedList.add(valueToAdd)
-
-linkedList.iterate()
-
-# removing a value
-itemToRemove = 2
-print('Removing from linked list: {}.'.format(itemToRemove))
-linkedList.removeFirstEquals(itemToRemove)
-
 linkedList.iterate()
 
 #removing the first value
 removedValue = linkedList.removeFirst()
 print('Removed first value: {}.'.format(removedValue))
-
-linkedList.iterate()
-
-insertValue = 6
-insertIndex = 2
-print('Inserting {} into index {}.'.format(insertValue, insertIndex))
-linkedList.insert(insertIndex, insertValue)
-
 linkedList.iterate()
 
 print('Linked list operation completed')
